d0772af96fbf8ef44e600aecfff9426b9148ea76
athulcoder <athul.sin.cos.tan@gmail.com>
2025-02-18 01:27:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const PORT = 8080;
+
+app.listen(PORT, () => console.log("App is lisening on PORT", PORT));
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to Hospital Dashboard</h1>");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1> Login page </h1>");
+});
+
+app.get("/api", (req, res) => {
+  res.json({ money: 300, title: "Athul" });
+});
