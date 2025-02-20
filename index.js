bf9d819c77cccce3d72f21caa5e0923234841a56
athulcoder <athul.sin.cos.tan@gmail.com>
2025-02-20 23:38:09 +0530

    new git

--- index.js
+++ index.js
@@ -9,11 +9,14 @@ app.get("/", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-  res.send("<h1> Login page </h1>");
+  res.send("<h1> Login page Sample Text page </h1>");
 });
 app.get("/dashboard", (req, res) => {
   res.send("<h1>Dashboard</h1>");
 });
 app.get("/api", (req, res) => {
   res.json({ money: 300, title: "Athul" });
+});
+app.get("/profile", (req, res) => {
+  res.send("<h1>Profile Page </h1>");
 });
